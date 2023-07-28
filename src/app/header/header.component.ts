129229f3bb7ca5ae7cadbe4ab52872b9f4eef09e
Mauricio Sotelo <mautsotelo@gmail.com>
2023-07-27 22:15:47 -0300

    initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  standalone: true,
+  imports: [MatIconModule]
+})
+export class HeaderComponent {
+
+}
